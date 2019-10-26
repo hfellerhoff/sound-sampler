4e032bffe28823232cdfebb28311e1f1856d5c83
Brianna Chou <bchou@luc.edu>
2019-10-26 18:09:54 -0500

    Added EmtpyLogo.jpg (Just the ring) Updated LoadingScreen.js

--- components/LoadingScreen.js
+++ components/LoadingScreen.js
@@ -1,15 +1,28 @@
 import React from 'react'
 
-import { View, StyleSheet } from 'react-native';
+import { View, StyleSheet, Image } from 'react-native';
+import { SCREEN_WIDTH } from '../constants/Sizes';
 
 const LoadingScreen = (props) => {
-    return <View style={styles.background} />
+    return (
+        <View style={styles.background}>
+            <Image style={styles.image} source={require('../assets/EmptyLogo.jpg')}/>
+        </View>
+        
+    )
 }
 
+const imageSize = SCREEN_WIDTH / 1.25;
 const styles = StyleSheet.create({
     background: {
         flex: 1,
-        backgroundColor: 'black'
+        backgroundColor: '#000000',
+        alignItems: 'center',
+        justifyContent: 'center'
+    },
+    image: {
+        width: imageSize,
+        height: imageSize * 1.5
     }
 })
 

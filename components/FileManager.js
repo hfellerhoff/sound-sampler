9e08b92f3e23dc5049ea38d9ced71e099b3d03c9
jnazemi <jnazemi@luc.edu>
2019-10-26 15:57:14 -0500

    Fix Async

--- components/FileManager.js
+++ components/FileManager.js
@@ -1,63 +1,63 @@
-import React, { useState, useEffect } from 'react';
-import FileDisplay from './FileDisplay';
-import { DUMMY_FILES } from '../constants/Dummy';
+import React, { useState, useEffect } from "react";
+import FileDisplay from "./FileDisplay";
+import { DUMMY_FILES } from "../constants/Dummy";
 
-const FileManager = (props) => {
-	const [ files, setFiles ] = useState(DUMMY_FILES);
+const FileManager = props => {
+  const [files, setFiles] = useState(DUMMY_FILES);
 
-	const getDirectory = (uri) => {
-		//Need to check to make sure URI is a valid directory
-		return(makeFileList(uri));
-	};
-	const getFile = async(uri) => {
-		//Need to run check to make sure URI is valid 
-		//Need to throw exception if invalid
-		const soundObject = new Audio.soundO();
-		await soundObject.loadAsync({
-			uri: FileSystem.documentDirectory + "small.mp4"
-		  });
-		return soundObject;
-	};
+  const getDirectory = uri => {
+    //Need to check to make sure URI is a valid directory
+    return makeFileList(uri);
+  };
+  const getFile = async uri => {
+    //Need to run check to make sure URI is valid
+    //Need to throw exception if invalid
+    const soundObject = new Audio.soundO();
+    await soundObject.loadAsync({
+      uri: FileSystem.documentDirectory + "small.mp4"
+    });
+    return soundObject;
+  };
 
-	const makeFileList = (uri) => //Default uri is 'FileSystem.documentDirectory'
-	{
-		let tempData = [];
-		await FileSystem.readDirectoryAsync(uri).then(
-		  data => {
-			data.forEach(file => {
-			  tempData.push({
-				name: file,
-				uri: FileSystem.documentDirectory + file
-				//Eventually add isDirectory 
-			  });
-			});
-		  }
-		);
-		return tempData;
-	}
+  const makeFileList = async (
+    uri //Default uri is 'FileSystem.documentDirectory'
+  ) => {
+    let tempData = [];
+    await FileSystem.readDirectoryAsync(uri).then(data => {
+      data.forEach(file => {
+        tempData.push({
+          name: file,
+          uri: FileSystem.documentDirectory + file
+          //Eventually add isDirectory
+        });
+      });
+    });
+    return tempData;
+  };
 
-	const moveFile = (oldUri, newUri) =>
-	{
-		const options = {
-			from: oldUri,
-			to: newUri
-		  };
-		  await FileSystem.moveAsync(options);
-	}
+  const moveFile = async (oldUri, newUri) => {
+    const options = {
+      from: oldUri,
+      to: newUri
+    };
+    await FileSystem.moveAsync(options);
+  };
 
-	useEffect(() => {
-		setFiles(makeFileList(FileSystem.documentDirectory));
-	}, [])
+  useEffect(() => {
+    setFiles(makeFileList(FileSystem.documentDirectory));
+  }, []);
 
-// useEffect(() =>
-// {
-// 	if(!isRecording)
-// 	{
-// 		pullCache();
-// 	}
-// }, [props.isRecording]);
+  // useEffect(() =>
+  // {
+  // 	if(!isRecording)
+  // 	{
+  // 		pullCache();
+  // 	}
+  // }, [props.isRecording]);
 
-	return <FileDisplay files={files} getDirectory={getDirectory} getFile={getFile} />;
+  return (
+    <FileDisplay files={files} getDirectory={getDirectory} getFile={getFile} />
+  );
 };
 
 export default FileManager;

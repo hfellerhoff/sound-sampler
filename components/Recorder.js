91e445c94d6a8239ec6971a5971c1d90d087e5f0
Alexander Rose <acrosemail@gmail.com>
2019-10-26 23:44:36 -0500

    Adding timestamps for Recording and playback

--- components/Recorder.js
+++ components/Recorder.js
@@ -1,11 +1,12 @@
-import React, { useState, useEffect } from "react";
-import { Audio } from "expo-av";
+import React, {useState, useEffect} from "react";
+import {Audio} from "expo-av";
 import * as Permissions from "expo-permissions";
 import * as FileSystem from "expo-file-system";
 import RecorderDisplay from "./RecorderDisplay";
+import View from "react-native-web/dist/exports/View";
 
 const Recorder = props => {
-  const { isRecording, setIsRecording } = props;
+  const {isRecording, setIsRecording} = props;
   const [recording, setRecording] = useState(null);
 
   const [hasRecordingPermissions, setHasRecordingPermissions] = useState(false);
@@ -16,7 +17,7 @@ const Recorder = props => {
   };
 
   const askForPermissions = async () => {
-    const { status } = await Permissions.askAsync(Permissions.AUDIO_RECORDING);
+    const {status} = await Permissions.askAsync(Permissions.AUDIO_RECORDING);
     setHasRecordingPermissions(status === "granted");
   };
 
@@ -38,7 +39,7 @@ const Recorder = props => {
         if (recording !== null) setRecording(null);
         const localRecording = new Audio.Recording();
         await localRecording.prepareToRecordAsync(
-          Audio.RECORDING_OPTIONS_PRESET_HIGH_QUALITY
+            Audio.RECORDING_OPTIONS_PRESET_HIGH_QUALITY
         );
         setRecording(localRecording);
         await localRecording.startAsync();
@@ -48,27 +49,48 @@ const Recorder = props => {
       }
     } else {
       alert(
-        "Please enable Sampler to use your microphone, as the app will not work otherwise."
+          "Please enable Sampler to use your microphone, as the app will not work otherwise."
       );
     }
   };
 
   const stopRecording = async () => {
     await recording
-      .stopAndUnloadAsync()
-      .then(setIsRecording(false))
-      .catch(() => {
-        alert(error);
-        setIsRecording(false);
-      });
+        .stopAndUnloadAsync()
+        .then(setIsRecording(false))
+        .catch(() => {
+          alert(error);
+          setIsRecording(false);
+        });
     const info = await FileSystem.getInfoAsync(recording.getURI());
   };
+  //used for getRecordingTimestamp
+  const getMMSSFromMillis = (props) => {
+    const {millis} = props;
+    const totalSeconds = millis / 1000;
+    const seconds = Math.floor(totalSeconds % 60);
+    const minutes = Math.floor(totalSeconds / 60);
+
+    const padWithZero = number => {
+      const string = number.toString();
+      if (number < 10) {
+        return '0' + string;
+      }
+      return string;
+    };
+    return padWithZero(minutes) + ':' + padWithZero(seconds);
+  };
+  const getRecordingTimestamp = async () => {
+    if (setIsRecording != null) {
+      return getMMSSFromMillis(setIsRecording.durationMillis)
+    }
+    return getMMSSFromMillis(0)
+  };
+
 
   useEffect(() => {
     askForPermissions();
   }, []);
-
-  return <RecorderDisplay isRecording={isRecording} onPress={onRecordPress} />;
+  return <RecorderDisplay isRecording={isRecording} onPress={onRecordPress}/>
 };
-
 export default Recorder;

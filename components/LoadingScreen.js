94a93a4d5e3f3bcc8828397d730ab40c295de74a
Brianna Chou <bchou@luc.edu>
2019-10-26 20:00:15 -0500

    Updating LoadingScreen.js

--- components/LoadingScreen.js
+++ components/LoadingScreen.js
@@ -1,12 +1,13 @@
 import React from 'react'
 
-import { View, StyleSheet, Image } from 'react-native';
-import { SCREEN_WIDTH } from '../constants/Sizes';
+import { View, StyleSheet, Image, Text } from 'react-native';
+import { SCREEN_WIDTH, SCREEN_HEIGHT } from '../constants/Sizes';
 
 const LoadingScreen = (props) => {
     return (
         <View style={styles.background}>
-            <Image style={styles.image} source={require('../assets/EmptyLogo.jpg')}/>
+            <Image style={styles.image} source={require('../assets/EmptyLogo.jpg')} />
+            <Text style={styles.text} >EARWORM</Text>
         </View>
         
     )
@@ -18,12 +19,37 @@ const styles = StyleSheet.create({
         flex: 1,
         backgroundColor: '#000000',
         alignItems: 'center',
-        justifyContent: 'center'
+        justifyContent: 'center',
+        marginTop: 20
     },
     image: {
         width: imageSize,
         height: imageSize * 1.5
+    },
+    text: {
+        position: 'absolute',
+        top: SCREEN_HEIGHT / 2 - 48,
+        left: 0,
+        right: 0,
+        color: '#02fffe',
+        fontSize: 18,
+        fontWeight: '400',
+        fontFamily: 'Arial',
+        textAlign: 'center',
+        letterSpacing: 5
     }
 })
-
+/* if (isLoading) {
+    return (<LoadingScreen />);
+}
+else {
+    return (
+    <View style={styles.container}>
+        <StatusBar barStyle="light-content" />
+        <Header title="Files" />
+        <FileManager isRecording={isRecording} />
+        <Recorder isRecording={isRecording} setIsRecording={setIsRecording} />
+    </View>
+    );
+} */
 export default LoadingScreen

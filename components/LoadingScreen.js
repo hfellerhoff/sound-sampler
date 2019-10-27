2f610b968bfb6207555889d2600af586969d68d3
Brianna Chou <bchou@luc.edu>
2019-10-26 23:23:06 -0500

    PULSING

--- components/LoadingScreen.js
+++ components/LoadingScreen.js
@@ -1,13 +1,46 @@
-import React from 'react';
+import React, { useEffect, useState} from 'react';
 
 import { View, StyleSheet, Image, Text, TouchableWithoutFeedback } from 'react-native';
 import { SCREEN_WIDTH, SCREEN_HEIGHT } from '../constants/Sizes';
+import Animated, { Easing } from 'react-native-reanimated'
+import { useTransition, bInterpolate } from "react-native-redash";
+// atan2(<Image style={styles.image} source={require('../assets/Innerpulse.jpg')} />, 
+//  		<Image style={styles.image} source={require('../assets/outerPulse.jpg')} />);
 
 const LoadingScreen = ({ onPress }) => {
+	const [changeLoop, setChangeLoop] = useState(false);
+
+	const keyboardTransition = useTransition(
+		changeLoop,
+		changeLoop ? 0 : 1,
+		changeLoop ? 1 : 0,
+		1000,
+		Easing.inOut(Easing.ease)
+	);
+	const innerImageScale = bInterpolate(keyboardTransition, 1.1 * SCREEN_WIDTH / 1.25, SCREEN_WIDTH / 1.25);
+	const innerTransitionStyle = {
+		width: innerImageScale,
+		height: innerImageScale,
+	}
+
+	// const outerImageScale = bInterpolate(keyboardTransition, SCREEN_WIDTH / 1.25, 1.1 * SCREEN_WIDTH / 1.25);
+	// const outerTransitionStyle = {
+	// 	width: outerImageScale,
+	// 	height: outerImageScale,
+	// }
+
+	useEffect(() => {
+		setTimeout(() => {
+			setChangeLoop(!changeLoop)
+		}, 1000);
+	}, [changeLoop])
+
 	return (
 		<TouchableWithoutFeedback onPress={onPress}>
 			<View style={styles.background}>
-				<Image style={styles.image} source={require('../assets/EmptyLogo.jpg')} />
+					<Animated.Image style={[styles.image, innerTransitionStyle]} source={require('../assets/EmptyLogo.jpg')} />
+					{/* <Animated.Image style={[styles.image, outerTransitionStyle]} source={require('../assets/outerpulse.jpg')} /> */}
+				
 				<Text style={styles.text}>EARWORM</Text>
 			</View>
 		</TouchableWithoutFeedback>
@@ -24,6 +57,10 @@ const styles = StyleSheet.create({
 		marginTop: 20
 	},
 	image: {
+		// position: "absolute",
+		top: 0,
+		left: 0,
+		// left: (SCREEN_WIDTH - imageSize * 1.1) / 2,
 		width: imageSize,
 		height: imageSize * 1.5
 	},
@@ -40,22 +77,4 @@ const styles = StyleSheet.create({
 		letterSpacing: 5
 	}
 });
-/* const [ isLoading, setIsLoading ] = useState(true);
-
-if (isLoading) {
-    return (
-        <LoadingScreen>
-        </LoadingScreen>
-    );
-}
-else {
-    return (
-    <View style={styles.container}>
-        <StatusBar barStyle="light-content" />
-        <Header title="Files" />
-        <FileManager isRecording={isRecording} />
-        <Recorder isRecording={isRecording} setIsRecording={setIsRecording} />
-    </View>
-    );
-} */
 export default LoadingScreen;

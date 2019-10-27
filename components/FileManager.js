60f42fb76cfadad23afdf3482c5c2fde0bfcff74
jnazemi <jnazemi@luc.edu>
2019-10-27 07:40:43 -0500

    yay

--- components/FileManager.js
+++ components/FileManager.js
@@ -1,168 +1,166 @@
-import React, { useState, useEffect } from 'react';
-import { Platform } from 'react-native';
-import FileDisplay from './FileDisplay';
-import { DUMMY_FILES } from '../constants/Dummy';
-import * as FileSystem from 'expo-file-system';
-import * as Sharing from 'expo-sharing';
+import React, { useState, useEffect } from "react";
+import { Platform } from "react-native";
+import FileDisplay from "./FileDisplay";
+import { DUMMY_FILES } from "../constants/Dummy";
+import * as FileSystem from "expo-file-system";
+import * as Sharing from "expo-sharing";
 
-const FileManager = (props) => {
-	const [ files, setFiles ] = useState([]);
-	const { movingOptions, setMovingOptions } = props;
+const FileManager = props => {
+  const [files, setFiles] = useState([]);
+  const { movingOptions, setMovingOptions } = props;
 
-	const changeName = async (oldUri, newName) => {
-		for (const file of files) {
-			if (oldUri === file.uri) {
-				let newTo = oldUri.replace(file.name, '');
-				const options = {
-					from: oldUri,
-					to: newTo + newName
-				};
+  const exportData = async uri => {
+    Sharing.shareAsync(uri);
+  };
 
-				await FileSystem.copyAsync(options).then(() => {
-					FileSystem.deleteAsync(oldUri).then(() => {
-						updateFiles();
-					});
-				});
-			}
-		}
-	};
+  const changeName = async (oldUri, newName) => {
+    for (const file of files) {
+      if (oldUri === file.uri) {
+        let newTo = oldUri.replace(file.name, "");
+        const options = {
+          from: oldUri,
+          to: newTo + newName
+        };
 
-	const getDirectory = (uri) => {
-		return makeFileList(uri);
-	};
+        await FileSystem.copyAsync(options).then(() => {
+          FileSystem.deleteAsync(oldUri).then(() => {
+            updateFiles();
+          });
+        });
+      }
+    }
+  };
 
-	const getFile = async (soundUri) => {
-		const soundObject = new Audio.soundO();
-		await soundObject.loadAsync({
-			uri: soundUri
-		});
-		return soundObject;
-	};
+  const getDirectory = uri => {
+    return makeFileList(uri);
+  };
 
-	const createDirectory = async (uri, name) => {
-		console.log('Making directory:' + uri + name);
-		await FileSystem.makeDirectoryAsync(uri + name);
-	};
+  const getFile = async soundUri => {
+    const soundObject = new Audio.soundO();
+    await soundObject.loadAsync({
+      uri: soundUri
+    });
+    return soundObject;
+  };
 
-	const makeFileList = async (
-		uri //Default uri is 'FileSystem.documentDirectory'
-	) => {
-		let tempData = [];
-		await FileSystem.readDirectoryAsync(uri).then(async (data) => {
-			for (const file of data) {
-				await FileSystem.getInfoAsync(props.currentDirectory + file).then(async (fileInfo) => {
-					tempData.push({
-						name: file,
-						uri: props.currentDirectory + file,
-						isDirectory: fileInfo.isDirectory
-					});
-				});
-			}
-		});
-		return tempData;
-	};
+  const createDirectory = async (uri, name) => {
+    console.log("Making directory:" + uri + name);
+    await FileSystem.makeDirectoryAsync(uri + name);
+  };
 
-	const deleteAllFiles = async () => {
-		FileSystem.readDirectoryAsync(FileSystem.documentDirectory).then((data) => {
-			for (const file of data) {
-				FileSystem.deleteAsync(FileSystem.documentDirectory + file);
-			}
-		});
-	};
+  const makeFileList = async (
+    uri //Default uri is 'FileSystem.documentDirectory'
+  ) => {
+    let tempData = [];
+    await FileSystem.readDirectoryAsync(uri).then(async data => {
+      for (const file of data) {
+        await FileSystem.getInfoAsync(props.currentDirectory + file).then(
+          async fileInfo => {
+            tempData.push({
+              name: file,
+              uri: props.currentDirectory + file,
+              isDirectory: fileInfo.isDirectory
+            });
+          }
+        );
+      }
+    });
+    return tempData;
+  };
 
-	const deleteFile = async (uri) => {
-		await FileSystem.deleteAsync(uri);
-		setTimeout(() => updateFiles(), 250);
-	};
+  const deleteAllFiles = async () => {
+    FileSystem.readDirectoryAsync(FileSystem.documentDirectory).then(data => {
+      for (const file of data) {
+        FileSystem.deleteAsync(FileSystem.documentDirectory + file);
+      }
+    });
+  };
 
-	const moveFile = async (oldUri, newUri) => {
-		const options = {
-			from: oldUri,
-			to: newUri
-		};
-		await FileSystem.moveAsync(options);
-	};
+  const deleteFile = async uri => {
+    await FileSystem.deleteAsync(uri);
+    setTimeout(() => updateFiles(), 250);
+  };
 
-	const updateFiles = async () => {
-		await makeFileList(props.currentDirectory).then((
-			newFiles //props.currentDirectory
-		) => setFiles(newFiles));
-	};
+  const moveFile = async (oldUri, newUri) => {
+    const options = {
+      from: oldUri,
+      to: newUri
+    };
+    await FileSystem.moveAsync(options);
+  };
 
-	const pullCache = async (currentDirectory) => {
-		const audioDirectoryName = Platform.OS === 'ios' ? 'AV/' : 'Audio/';
-		const directoryName = FileSystem.cacheDirectory + audioDirectoryName;
+  const updateFiles = async () => {
+    await makeFileList(props.currentDirectory).then((
+      newFiles //props.currentDirectory
+    ) => setFiles(newFiles));
+  };
 
-		await FileSystem.readDirectoryAsync(directoryName).then((data) => {
-			for (const file of data) {
-				moveFile(directoryName + file, currentDirectory + file).then(() => {
-					updateFiles();
-				});
-			}
-		});
-	};
+  const pullCache = async currentDirectory => {
+    const audioDirectoryName = Platform.OS === "ios" ? "AV/" : "Audio/";
+    const directoryName = FileSystem.cacheDirectory + audioDirectoryName;
 
-	useEffect(
-		() => {
-			updateFiles();
-			props.setIsLoading();
-			// testFunction();
-		},
-		[ props.currentDirectory ]
-	); //props.currentDirectory
+    await FileSystem.readDirectoryAsync(directoryName).then(data => {
+      for (const file of data) {
+        moveFile(directoryName + file, currentDirectory + file).then(() => {
+          updateFiles();
+        });
+      }
+    });
+  };
 
-	useEffect(
-		() => {
-			if (props.shouldCreateNewDirectory) {
-				createDirectory(props.newDirectoryInformation.uri, props.newDirectoryInformation.name);
-				updateFiles();
-				props.onDirectoryCreate();
-			}
-		},
-		[ props.shouldCreateNewDirectory ]
-	);
+  useEffect(() => {
+    updateFiles();
+    props.setIsLoading();
+    // testFunction();
+  }, [props.currentDirectory]); //props.currentDirectory
 
-	useEffect(
-		() => {
-			if (!props.isRecording) {
-				pullCache(props.currentDirectory);
-			}
-		},
-		[ props.isRecording ]
-	);
+  useEffect(() => {
+    if (props.shouldCreateNewDirectory) {
+      createDirectory(
+        props.newDirectoryInformation.uri,
+        props.newDirectoryInformation.name
+      );
+      updateFiles();
+      props.onDirectoryCreate();
+    }
+  }, [props.shouldCreateNewDirectory]);
 
-	useEffect(
-		() => {
-			// console.log('checking...');
-			console.log(movingOptions);
-			if (movingOptions.toUri) {
-				console.log('Moving...');
-				moveFile(movingOptions.fromUri, movingOptions.toUri);
-				console.log('Moved.');
-				setMovingOptions({
-					areMoving: false,
-					fromUri: null,
-					toUri: null
-				});
-				console.log(movingOptions);
-				setTimeout(() => updateFiles(), 250);
-			}
-		},
-		[ movingOptions ]
-	);
+  useEffect(() => {
+    if (!props.isRecording) {
+      pullCache(props.currentDirectory);
+    }
+  }, [props.isRecording]);
 
-	return (
-		<FileDisplay
-			files={files}
-			getDirectory={getDirectory}
-			getFile={getFile}
-			deleteFile={deleteFile}
-			moveFile={moveFile}
-			currentDirectory={props.currentDirectory}
-			setCurrentDirectory={props.setCurrentDirectory}
-			movingOptions={movingOptions}
-			setMovingOptions={setMovingOptions}
-		/>
-	);
+  useEffect(() => {
+    // console.log('checking...');
+    console.log(movingOptions);
+    if (movingOptions.toUri) {
+      console.log("Moving...");
+      moveFile(movingOptions.fromUri, movingOptions.toUri);
+      console.log("Moved.");
+      setMovingOptions({
+        areMoving: false,
+        fromUri: null,
+        toUri: null
+      });
+      console.log(movingOptions);
+      setTimeout(() => updateFiles(), 250);
+    }
+  }, [movingOptions]);
+
+  return (
+    <FileDisplay
+      files={files}
+      getDirectory={getDirectory}
+      getFile={getFile}
+      deleteFile={deleteFile}
+      moveFile={moveFile}
+      currentDirectory={props.currentDirectory}
+      setCurrentDirectory={props.setCurrentDirectory}
+      movingOptions={movingOptions}
+      setMovingOptions={setMovingOptions}
+      exportData={exportData}
+    />
+  );
 };
 export default FileManager;
